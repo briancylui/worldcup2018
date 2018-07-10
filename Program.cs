6c2d5c34af90ca52a26256b310e2c22135d566e9
Brian Lui <brianlui@stanford.edu>
2018-07-09 17:04:57 -0700

    Implemented comparison across models with proper output
    Program output for testing only one model:
    The best models are:
    FastTreeRegressor
    -- numLeaves : 5
    -- learningRates : 0.1
    -- maxBins : 20
    -- England : 1.180031
    -- Sweden : 0.7405879
    -- loss : 1.25614987313573

--- Program.cs
+++ Program.cs
@@ -24,7 +24,51 @@ namespace WorldCup
 
         static async Task Main(string[] args)
         {
-            double best_loss = Double.MaxValue;
+            List<Dictionary<string, object>> models = new List<Dictionary<string, object>>();
+            Dictionary<string, object> bestFastTreeRegressor = await chooseBestFastTreeRegressor();
+            models.Add(bestFastTreeRegressor);
+
+            double min_loss = Double.MaxValue;
+            List<int> bestModelIndices = new List<int>();
+
+            for (int i = 0; i < models.Count; i++)
+            {
+                Dictionary<string, object> model = models[i];
+                double loss = (double)model["loss"];
+                if (loss < min_loss)
+                {
+                    bestModelIndices.Clear();
+                    bestModelIndices.Add(i);
+                    min_loss = loss;
+                }
+                else if (loss == min_loss)
+                {
+                    bestModelIndices.Add(i);
+                }
+            }
+
+            Console.WriteLine($"The best models are:");
+            foreach (int index in bestModelIndices)
+            {
+                Dictionary<string, object> model = models[index];
+                foreach (var item in model)
+                {
+                    if (item.Key == "type")
+                    {
+                        Console.WriteLine(item.Value);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"-- {item.Key} : {item.Value}");
+                    }
+                }
+            }
+        }
+
+        public static async Task<Dictionary<string, object>> chooseBestFastTreeRegressor()
+        {
+            double bestLoss = Double.MaxValue;
+            Dictionary<string, object> bestVersion = new Dictionary<string, object>();
 
             foreach (int numLeaves in numLeavesList)
             {
@@ -38,19 +82,24 @@ namespace WorldCup
                         WorldCupPrediction predictionForEngland = model.Predict(TestData.TestEngland);
                         WorldCupPrediction predictionForSweden = model.Predict(TestData.TestSweden);
 
-                        if (loss < best_loss)
+                        if (loss < bestLoss)
                         {
                             Console.WriteLine($"Best model so far is: NumLeaves = {numLeaves}, LearningRates = {learningRates}, MaxBins = {maxBins}, with loss (RMS) {loss}");
                             Console.WriteLine($"-- Predicted result is: England VS Sweden = {predictionForEngland.HomeTeamGoals} : {predictionForSweden.HomeTeamGoals}");
-                            best_loss = loss;
+                            bestLoss = loss;
+                            bestVersion["type"] = "FastTreeRegressor";
+                            bestVersion["numLeaves"] = numLeaves;
+                            bestVersion["learningRates"] = learningRates;
+                            bestVersion["maxBins"] = maxBins;
+                            bestVersion["England"] = predictionForEngland.HomeTeamGoals;
+                            bestVersion["Sweden"] = predictionForSweden.HomeTeamGoals;
+                            bestVersion["loss"] = loss;
                         }
                     }
                 }
             }
 
-
-
-
+            return bestVersion;
         }
 
         public static async Task<PredictionModel<WorldCupData, WorldCupPrediction>> Train(string type, Dictionary<string, object> kwargs = null)
